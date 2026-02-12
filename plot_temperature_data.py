86a2c130bd0f8d1691407b3249ca92010b13108e
Dmitry Nasonov <1260248+gvard@users.noreply.github.com>
2026-02-12 18:12:21 +0300

    now plots different data sets

--- plot_temperature_data.py
+++ plot_temperature_data.py
@@ -1,10 +1,11 @@
 """Python script for plotting temperature monitoring data
+Example usage:
+python plot_temperature_data.py data/260129-0204.log data/260206-08.log -o data/odtemp-260206-08.log data/odtemp-260208-1912.log
 """
 
-from datetime import datetime, timedelta
+from datetime import timedelta
 from argparse import ArgumentParser
 
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib.dates as mdates
@@ -13,55 +14,88 @@ from matplotlib.ticker import MultipleLocator
 parser = ArgumentParser(
     description="Python script for plotting temperature monitoring data"
 )
-parser.add_argument("filenames", type=str, default="", nargs='+', metavar='FILES',
+parser.add_argument("filenames", nargs="+", metavar="FILES",
+                    help="input CSV data file(s)")
+parser.add_argument("-o", "--odtfnames", nargs="+", metavar="ODTFILES",
                     help="input CSV data file(s)")
 args = parser.parse_args()
 
 FORMAT = "%d.%m.%Y %H:%M:%S"
-XMAJOR_LOCATOR = 4
-XMINOR_LOCATOR = 1
+XMAJOR_LOCATOR = 1
+XMINOR_LOCATOR = 6
+YMAJOR_LOCATOR = 1
 YMINOR_LOCATOR = 0.5
-columns = ["date", "time", "temp"]
+MS = 7
+DPI_SCREEN = 200
+DPI = 160
+columns = ["date", "time", "temp", "flag"]
+xlims = []
+extremes = []
 
 fig, ax = plt.subplots(figsize=(16, 9))
 
-xlims = []
-extremes = []
 for i, nam in enumerate(args.filenames):
     data = pd.read_csv(nam, header=0, names=columns)
-    data['datetime'] = pd.to_datetime(data['date'] + ' ' + data['time'], format=FORMAT)
-    xlims.append((data['datetime'].iloc[0], data['datetime'].iloc[-1]))
-    extremes.append(min(data['temp']))
-    extremes.append(max(data['temp']))
+    data["datetime"] = pd.to_datetime(data["date"] + " " + data["time"], format=FORMAT)
+    xlims.append(data["datetime"].iloc[0])
+    xlims.append(data["datetime"].iloc[-1])
+    extremes.append(min(data["temp"]))
+    extremes.append(max(data["temp"]))
     if not i:
-        plt.plot(data['datetime'], data['temp'], "-o", lw=1, color='blue', label='Температура с USB термодатчика')
+        plt.plot(data["datetime"], data["temp"], "ok", lw=1, label="Показания термометров")
+        datad = data[data["flag"] == "d"]
+        plt.plot(datad["datetime"], datad["temp"], "og", lw=1, markeredgecolor="k", mew=0.5, ms=MS, zorder=7,
+                 label="Показания термометра c фоторегистрацией")
     else:
-        plt.plot(data['datetime'], data['temp'], "-o", lw=1, color='blue')
+        plt.plot(data["datetime"], data["temp"], "ok", lw=1)
+        datad = data[data["flag"] == "d"]
+        plt.plot(datad["datetime"], datad["temp"], "og", lw=1, markeredgecolor="k", mew=0.5, ms=MS, zorder=7)
 
-temp_delta = 0.5
+columns = ["date", "time", "temp"]
+for i, nam in enumerate(args.odtfnames):
+    data = pd.read_csv(nam, header=0, names=columns)
+    data["datetime"] = pd.to_datetime(data["date"] + " " + data["time"], format=FORMAT)
+    xlims.append(data["datetime"].iloc[0])
+    xlims.append(data["datetime"].iloc[-1])
+    extremes.append(min(data["temp"]))
+    extremes.append(max(data["temp"]))
+    if not i:
+        plt.plot(data["datetime"], data["temp"], "-o", lw=1, color="blue", markeredgecolor="k", mew=0.5, ms=MS,
+                 label="Температура с USB термодатчика")
+    else:
+        plt.plot(data["datetime"], data["temp"], "-o", lw=1, color="blue", markeredgecolor="k", mew=0.5, ms=MS)
+
+temp_delta = 0.3
 ylims = (min(extremes) - temp_delta, max(extremes) + temp_delta)
 
-current_date = xlims[0][0].replace(hour=0, minute=0, second=0, microsecond=0)
-while current_date <= xlims[-1][-1]:
-        plt.plot((current_date, current_date), ylims, "-k", lw=2, zorder=-5)
-        current_date += timedelta(days=1)
+current_date = min(xlims).replace(hour=0, minute=0, second=0)
+day = timedelta(days=1)
+while current_date <= max(xlims) + day:
+        plt.plot((current_date, current_date), ylims, "--k", lw=1, zorder=-5)
+        current_date += day
 
-plt.title('Изменения температуры в помещении')
-plt.xlabel('Время, ДД ЧЧ:ММ')
-plt.ylabel('Температура, °C')
-plt.grid(True, linestyle='--', alpha=0.7)
+plt.title("Измерения температуры в помещении")
+plt.xlabel("Время, ДД ЧЧ:ММ", fontsize=12)
+plt.ylabel("Температура, °C", fontsize=12)
+plt.grid(True, linestyle="--", alpha=0.7)
 plt.tight_layout()
-td = timedelta(minutes=50)
-plt.xlim(xlims[0][0] - td, xlims[-1][-1] + td)
+td = timedelta(minutes=180)
+plt.xlim(min(xlims) - td, max(xlims) + 2*td)
 plt.ylim(ylims)
 plt.legend(loc="upper left")
 
-ax.xaxis.set_major_locator(mdates.HourLocator(interval=XMAJOR_LOCATOR))
-ax.xaxis.set_minor_locator(mdates.HourLocator(interval=XMINOR_LOCATOR))
-date_form = mdates.DateFormatter('%d %H:%M')
+ax.xaxis.set_major_locator(mdates.DayLocator(interval=XMAJOR_LOCATOR))
+date_form = mdates.DateFormatter("%d %H:%M")
 ax.xaxis.set_major_formatter(date_form)
+ax.minorticks_on()
+ax.xaxis.set_minor_locator(mdates.HourLocator(interval=XMINOR_LOCATOR))
+ax.yaxis.set_major_locator(MultipleLocator(YMAJOR_LOCATOR))
 ax.yaxis.set_minor_locator(MultipleLocator(YMINOR_LOCATOR))
 ax.tick_params(which="major", length=3.5)
 ax.tick_params(which="minor", length=2.5)
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+# fig.set_dpi(DPI_SCREEN)
+# plt.get_current_fig_manager().window.state('zoomed')
 # plt.show()
-plt.savefig("temperatures.png", dpi=160)
+plt.savefig("temperatures.png", dpi=DPI)
